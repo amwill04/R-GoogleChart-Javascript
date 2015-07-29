ff3036369966f5108c2517161f08cdce5ad888f6
amwill04 <alanwilliams@Alans-iMac.local>
2015-07-29 15:37:19 +0100

    First running function

--- GoogleChart_js.R
+++ GoogleChart_js.R
@@ -2,9 +2,16 @@ dynamic_gvis <- function(chart, file) {
 
   js <-c("google.load('visualization', '1', {'packages':['corechart']});\n")
 
-  for (x in 1:(length(chart)/3)) {
-    chart_type <- chart[[x]][1]$type
-    chart_id <- chart[[x]][1]$chartid
+  for (x in 1:length(chart)) {
+    if (class(chart)[1] == "gvis") {
+      chart_type <- chart$type
+      chart_id <- chart$chartid
+      chart_data <- chart$html$chart['jsData']
+    } else {
+      chart_type <- chart[[x]][1]$type
+      chart_id <- chart[[x]][2]$chartid
+      chart_data <- chart[[x]][3]$html$chart['jsData']
+    }
     chart_api_load <- paste("google.setOnLoadCallback(drawChart", chart_id, ");", sep = "")
     chart_draw <- paste("function drawChart", chart_id, "() {
                            var data = gvisData", chart_id, "();
@@ -21,7 +28,7 @@ dynamic_gvis <- function(chart, file) {
                                });
 
                           }", sep = "")
-    google_chart <- paste(chart_api_load, chart_draw, sep = "\n")
+    google_chart <- paste(chart_api_load, chart_data,chart_draw, sep = "\n")
 
     js <- c(js, google_chart)
 
